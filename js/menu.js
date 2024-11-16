11ecc9476d4414e785ef1a9d699c8c49c1d05e4b
Sarah Penrose <penroses003@gmail.com>
2024-11-16 17:26:36 -0500

    homepage

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector(".menu");
+const menu = document.querySelector("#menu");
 const menuItems = document.querySelectorAll(".menu-item");
 const hamburger= document.querySelector(".hamburger");
 const closeIcon= document.querySelector(".close-icon");
